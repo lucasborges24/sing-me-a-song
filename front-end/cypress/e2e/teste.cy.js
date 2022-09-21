2a9d1f2129596e1b6f0c20ba958c18a9612084cd
lucas b <lucasborgesbarbosa2012@gmail.com>
2022-09-21 11:15:17 -0300

    chore: init cypress

---
+++ front-end/cypress/e2e/teste.cy.js
@@ -0,0 +1,12 @@
+describe("Login", () => {
+  it("should login successfully", () => {
+    // eslint-disable-next-line no-undef
+    cy.visit("http://localhost:3000");
+
+    // cy.get("input[type=text]").type("usuario@email.com");
+    // cy.get("input[type=password").type("senha_super_secreta");
+    // cy.get(".submit").click();
+
+    // cy.url().should("equal", "http://localhost:3000/pagina-protegida");
+  });
+});
