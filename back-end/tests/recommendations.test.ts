3664ab2147bf45cc996be9b25078bcafda413d30
lucas b <lucasborgesbarbosa2012@gmail.com>
2022-09-21 11:12:38 -0300

    feat: add test to POST recommendation

--- back-end/tests/recommendations.test.ts
+++ back-end/tests/recommendations.test.ts
@@ -1,6 +1,13 @@
 import supertest from "supertest";
 import { faker } from "@faker-js/faker";
 import app from "../src/app";
+import {
+  deleteAllData,
+  disconnectPrisma,
+  ScenarioWithWhateverYouWantRecommendations,
+} from "./factories/scenarioFactory";
+import { prisma } from "../src/database";
+import { newRecommendationFaker } from "./factories/fakerObjects";
 
 beforeEach(async () => {
   await deleteAllData();
@@ -12,3 +19,17 @@ afterAll(async () => {
 
 const server = supertest(app);
 
+describe("POST /recommendations", () => {
+  it("Should create a new song recommendation with status 201", async () => {
+    const newSong = newRecommendationFaker();
+    const result = await server.post("/recommendations").send(newSong);
+    const createdSong = await prisma.recommendation.findFirst({
+      where: {
+        name: newSong.name,
+        youtubeLink: newSong.youtubeLink,
+      },
+    });
+    expect(result.status).toBe(201);
+    expect(createdSong).not.toBeNull();
+  });
+});
