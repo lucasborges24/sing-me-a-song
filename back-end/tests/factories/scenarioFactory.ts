a72a60c97d9079da31ef9944fe4b0fcca949b271
lucas b <lucasborgesbarbosa2012@gmail.com>
2022-09-21 11:10:35 -0300

    feat: create scenario for add x news recommendations

--- back-end/tests/factories/scenarioFactory.ts
+++ back-end/tests/factories/scenarioFactory.ts
@@ -1,4 +1,6 @@
 import { prisma } from "../../src/database";
+import { newRecommendationFaker } from "./fakerObjects";
+
 export const deleteAllData = async () => {
   await prisma.$transaction([
     prisma.$executeRaw`TRUNCATE TABLE recommendations`,
@@ -7,4 +9,16 @@ export const deleteAllData = async () => {
 
 export const disconnectPrisma = async () => {
   await prisma.$disconnect();
+};
+
+export const ScenarioWithWhateverYouWantRecommendations = async (newRecommendations: number) => {
+  const newRecommendationsArr = [];
+  for (let i = 0; i < newRecommendations; i++) {
+    const newSong = newRecommendationFaker();
+    newRecommendationsArr.push(newSong);
+  }
+  await prisma.recommendation.createMany({
+    data: newRecommendationsArr,
+  });
+  return;
 };
