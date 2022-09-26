fca9d0be00c51fce82972368a237c30618cd9bcd
lucas b <lucasborgesbarbosa2012@gmail.com>
2022-09-26 11:08:23 -0300

    feat: create scnearios

--- back-end/tests/factories/scenarioFactory.ts
+++ back-end/tests/factories/scenarioFactory.ts
@@ -1,5 +1,6 @@
 import { prisma } from "../../src/database";
 import { newRecommendationFaker } from "./fakerObjects";
+import { recommendationWithScoreByParam } from "./recommendationFactory";
 
 export const deleteAllData = async () => {
   await prisma.$transaction([
@@ -11,7 +12,9 @@ export const disconnectPrisma = async () => {
   await prisma.$disconnect();
 };
 
-export const ScenarioWithWhateverYouWantRecommendations = async (newRecommendations: number) => {
+export const ScenarioWithWhateverYouWantRecommendations = async (
+  newRecommendations: number
+) => {
   const newRecommendationsArr = [];
   for (let i = 0; i < newRecommendations; i++) {
     const newSong = newRecommendationFaker();
@@ -21,4 +24,59 @@ export const ScenarioWithWhateverYouWantRecommendations = async (newRecommendati
     data: newRecommendationsArr,
   });
   return;
+};
+
+export const scenarioOneRecommendation = async () => {
+  const song = newRecommendationFaker();
+  const data = await prisma.recommendation.create({
+    data: song,
+  });
+  return data;
+};
+
+export const ScenarioWithWhateverYouWantRecommendationsWithParamScore = async (
+  newRecommendation: number
+) => {
+  const newRecommendationsArr = [];
+  for (let i = newRecommendation; i > 0; i--) {
+    const recommendation = recommendationWithScoreByParam(i);
+    newRecommendationsArr.push(recommendation);
+  }
+  const data = await prisma.recommendation.createMany({
+    data: newRecommendationsArr,
+  });
+  return data;
+};
+
+export const scenarioRecommendationsWithScoreLessThanTen = async (
+  recommendations: number
+) => {
+  const newRecommendationsArr = [];
+  for (let i = 0; i < recommendations; i++) {
+    const randomScore = Math.ceil((Math.random() - 0.5) * 10);
+    const recommendation = recommendationWithScoreByParam(randomScore);
+    newRecommendationsArr.push(recommendation);
+  }
+  const data = await prisma.recommendation.createMany({
+    data: newRecommendationsArr,
+  });
+  return data;
+};
+
+export const scenarioRecommendationsWithScoreGreaterThanTen = async (
+  recommendations: number
+) => {
+  const newRecommendationsArr = [];
+  for (let i = 0; i < recommendations; i++) {
+    const randomScore = Math.floor(Math.random() * (Math.random() * 100));
+    const randomScoreGreaterThanTen = randomScore > 10 ? randomScore : 11;
+    const recommendation = recommendationWithScoreByParam(
+      randomScoreGreaterThanTen
+    );
+    newRecommendationsArr.push(recommendation);
+  }
+  const data = await prisma.recommendation.createMany({
+    data: newRecommendationsArr,
+  });
+  return data;
 };
