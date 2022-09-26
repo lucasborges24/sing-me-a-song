770ff8ee1a5679e1f3e803db23414c8effd91c46
lucas b <lucasborgesbarbosa2012@gmail.com>
2022-09-26 11:05:17 -0300

    chore: look only for src/services

--- back-end/jest.config.js
+++ back-end/jest.config.js
@@ -11,4 +11,17 @@ export default {
   moduleNameMapper: {
     "^(\\.{1,2}/.*)\\.js$": "$1",
   },
+  collectCoverage: true,
+  setupFilesAfterEnv: ["./jest.setup.ts"],
+  coveragePathIgnorePatterns: [
+    '<rootDir>/src/app.ts',
+    '<rootDir>/src/database.ts',
+    '<rootDir>/src/utils',
+    '<rootDir>/src/middlewares',
+    '<rootDir>/src/controllers',
+    '<rootDir>/src/repositories',
+    '<rootDir>/src/routers',
+    '<rootDir>/src/schemas',
+    '<rootDir>/tests/factories',
+]
 };
