7508209925cbd401e6e0d9b7e94589e014ddcf4b
lucas b <lucasborgesbarbosa2012@gmail.com>
2022-09-26 11:07:45 -0300

    fix: replace song to lorem

--- back-end/tests/factories/fakerObjects.ts
+++ back-end/tests/factories/fakerObjects.ts
@@ -2,10 +2,11 @@ import { faker } from "@faker-js/faker";
 
 export const newRecommendationFaker = () => {
   const recommendation = {
-    name: faker.music.songName(),
+    name: faker.lorem.words(3),
     youtubeLink: `https://www.youtube.com/watch?v=${faker.internet.password(
       11
     )}`,
   };
   return recommendation;
 };
+
